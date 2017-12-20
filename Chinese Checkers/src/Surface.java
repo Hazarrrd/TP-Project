3b52d0b9d9133860ac364335bc6aa950fcda4564
lenovo <lenovo@LAPTOP-AI932HNH>
2017-12-20 16:57:57 +0100

    window

---
+++ Chinese Checkers/src/Surface.java
@@ -0,0 +1,41 @@
+package chess;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Surface extends JPanel{
+	
+	Image image;
+	//Board board;
+	
+	public Surface(Image image, int type) 
+	{
+	    this.image = image;
+	   
+	    setPreferredSize(new Dimension(600,600));
+		
+	   // board = new Board();
+	   
+		repaint();
+	}
+	
+	private void doDrawing(Graphics g) {
+	      
+	  	Graphics2D g2d = (Graphics2D) g;
+	  	
+	}
+	
+	 public void paintComponent(Graphics g) {
+		  	
+		  	g.drawImage(image, 0, 0, null);
+		  	doDrawing(g);        
+		  }
+	
+}
---
+++ Chinese Checkers/src/Window.java
@@ -0,0 +1,67 @@
+package chess;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+class Window extends Frame implements ActionListener{
+	
+	TextField input;
+	Button button;
+	Surface output;
+	
+	int type;
+	
+	public Window(int type) {
+		setResizable(false);
+		this.type = type;
+	
+	    output = new Surface(new ImageIcon("checkers.jpg").getImage(), type);
+	   
+	    setLayout(new BorderLayout());
+	    
+	    JPanel menu = new JPanel();
+	    menu.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
+	    
+	    add(menu, BorderLayout.NORTH);
+	    add(output, BorderLayout.CENTER);
+	    
+	    setBounds(100,100,600,400);
+	    pack();
+	}
+	
+	public void actionPerformed(ActionEvent event) {
+	    
+		
+	}
+	
+	public static void main(String[] args) {
+  		
+  		Object[] options = {"OK", "Zamknij"};
+  		
+  		int type = JOptionPane.showOptionDialog(null, "Wybierz ilosc graczy", "Chinese checkers", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, null);
+
+  		if(type == 0)
+  		{
+  			Window frame = new Window(type);
+  			frame.setTitle("Chinese checkers");
+  			
+  			frame.addWindowListener( new WindowAdapter() {
+  			
+  				public void windowClosing(WindowEvent e) {
+  					System.exit(0);
+  				}
+  			});
+  			
+  			frame.setVisible(true);
+  			//frame.listenSocket();
+  		}
+  		else {
+  			System.exit(0);
+  		}
+  	}
+}
